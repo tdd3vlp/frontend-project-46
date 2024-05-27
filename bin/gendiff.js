d37ac18043601a224f6e1ca9ba6da73c99a4e40b
tdd3vlp <tdd3vlp@gmail.com>
2024-05-27 14:25:50 +0300

    fix: change project structure, complete reading and parsing functionality

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,22 +1,16 @@
 #!/usr/bin/env node
 
+import genDiff from '../src/index.js';
 import { program } from 'commander';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import { parseData } from '../src/index.js';
+
+const fn = (filepath1, filepath2) => {
+  console.log(genDiff(filepath1, filepath2));
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filePath1, filePath2) => {
-    const absolutePath1 = path.resolve(process.cwd(), filePath1);
-    const absolutePath2 = path.resolve(process.cwd(), filePath2);
-
-    const firstFile = parseData(fs.readFileSync(absolutePath1));
-    const secondFile = parseData(fs.readFileSync(absolutePath2));
-
-    return { firstFile, secondFile };
-  })
+  .action(fn)
   .parse();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export const parseData = (data) => {
-  return JSON.parse(data);
+import { readFileSync } from 'node:fs';
+import * as path from 'node:path';
+import parse from './parsers.js';
+
+const getFileData = (file) => {
+  return readFileSync(path.resolve(file), 'utf-8');
 };
+
+const genDiff = (filePath1, filePath2) => {
+  const firstFile = parse(getFileData(filePath1));
+  const secondFile = parse(getFileData(filePath2));
+
+  return { firstFile, secondFile };
+};
+
+export default genDiff;
