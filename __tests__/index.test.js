45e9d443be143f82cd060ee978affa960d3f2cb0
tdd3vlp <tdd3vlp@gmail.com>
2024-08-11 14:41:15 +0300

    test: compare two flat files

---
+++ __tests__/index.test.js
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import * as path from "node:path";
+import { fileURLToPath } from "url";
+import gendiff from "../src/index.js";
+import { describe, expect, test } from "@jest/globals";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+describe("Compare module", () => {
+  test("Compare two flat files", () => {
+    expect(
+      gendiff(
+        path.join(__dirname, "..", "__fixtures__", "file1.json"),
+        path.join(__dirname, "..", "__fixtures__", "file2.json"),
+      ),
+    ).toEqual(result);
+  });
+});
