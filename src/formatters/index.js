cb098d339957c7a71eb9457b4e4634ced663ee94
tdd3vlp <tdd3vlp@gmail.com>
2024-09-14 19:14:50 +0300

    feat: realize json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,6 @@
 import stylishFormatter from './stylishFormatter.js';
 import plainFormatter from './plainFormatter.js';
+import jsonFormatter from './jsonFormatter.js';
 
 const format = (tree, formatName) => {
   switch (formatName) {
@@ -7,6 +8,8 @@ const format = (tree, formatName) => {
       return stylishFormatter(tree);
     case 'plain':
       return plainFormatter(tree);
+    case 'json':
+      return jsonFormatter(tree);
     default:
       throw new Error(`Unknown formatter: ${formatName}`);
   }
---
+++ src/formatters/jsonFormatter.js
@@ -0,0 +1,34 @@
+const jsonFormatter = (diffTree) => {
+  const formatItem = (item) => {
+    switch (item.type) {
+      case 'nested':
+        return { [item.key]: formatItems(item.value) };
+      case 'added':
+        return { [item.key]: { type: 'added', value: item.value } };
+      case 'removed':
+        return { [item.key]: { type: 'removed', value: item.value } };
+      case 'unchanged':
+        return { [item.key]: { type: 'unchanged', value: item.value } };
+      case 'changed':
+        return {
+          [item.key]: {
+            type: 'changed',
+            oldValue: item.oldValue,
+            newValue: item.newValue,
+          },
+        };
+      default:
+        throw new Error(`Unknown item type: ${item.type}`);
+    }
+  };
+
+  const formatItems = (items) => {
+    return items.reduce((acc, item) => {
+      return { ...acc, ...formatItem(item) };
+    }, {});
+  };
+
+  return JSON.stringify(formatItems(diffTree), null, 2);
+};
+
+export default jsonFormatter;
