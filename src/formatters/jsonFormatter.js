041fad154261043f3a762651ab70c8e7d78ee41a
tdd3vlp <tdd3vlp@gmail.com>
2024-09-15 13:19:35 +0300

    fix: edit jsonFormatter.js

--- src/formatters/jsonFormatter.js
+++ src/formatters/jsonFormatter.js
@@ -9,28 +9,28 @@ const formatValue = (value) => {
 
 const jsonFormatter = (tree) => {
   const formatItems = (items) => items.reduce((acc, item) => {
-      switch (item.type) {
-        case 'nested':
-          return { ...acc, [item.key]: formatItems(item.value) };
-        case 'added':
-          return { ...acc, [item.key]: { type: 'added', value: formatValue(item.value) } };
-        case 'removed':
-          return { ...acc, [item.key]: { type: 'removed', value: formatValue(item.value) } };
-        case 'unchanged':
-          return { ...acc, [item.key]: { type: 'unchanged', value: formatValue(item.value) } };
-        case 'changed':
-          return {
-            ...acc,
-            [item.key]: {
-              type: 'changed',
-              oldValue: formatValue(item.oldValue),
-              newValue: formatValue(item.newValue),
-            },
-          };
-        default:
-          throw new Error(`Unknown item type: ${item.type}`);
-      }
-    }, {});
+    switch (item.type) {
+      case 'nested':
+        return { ...acc, [item.key]: formatItems(item.value) };
+      case 'added':
+        return { ...acc, [item.key]: { type: 'added', value: formatValue(item.value) } };
+      case 'removed':
+        return { ...acc, [item.key]: { type: 'removed', value: formatValue(item.value) } };
+      case 'unchanged':
+        return { ...acc, [item.key]: { type: 'unchanged', value: formatValue(item.value) } };
+      case 'changed':
+        return {
+          ...acc,
+          [item.key]: {
+            type: 'changed',
+            oldValue: formatValue(item.oldValue),
+            newValue: formatValue(item.newValue),
+          },
+        };
+      default:
+        throw new Error(`Unknown item type: ${item.type}`);
+    }
+  }, {});
 
   return JSON.stringify(formatItems(tree), null, 2);
 };
